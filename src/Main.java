110a5373933b707afb4e98f16b8928c07c521f1f
LucasWillBlumenau <109769158+LucasWillBlumenau@users.noreply.github.com>
2022-10-23 00:26:43 -0300

    algumas alterações na aparência do programa

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,8 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/TelaCalculadora.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
+        scene.getStylesheets().add(getClass().getResource("/view/styles.css").toExternalForm());
+        stage.setResizable(false);
         stage.setTitle("Calculadora");
         stage.setScene(scene);
         stage.show();
